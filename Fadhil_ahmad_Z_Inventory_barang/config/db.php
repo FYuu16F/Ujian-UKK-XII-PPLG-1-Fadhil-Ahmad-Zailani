6fea9ca28c7c3f38f05cba59b3ba14930d7b50af
FYuu16F <dewi85492@gmail.com>
2025-11-18 07:25:39 +0700

    Delete Fadhil_ahmad_Z_Inventory_barang directory

--- Fadhil_ahmad_Z_Inventory_barang/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "fadhil_Ahmad_Z_inventory_db";
-
-$conn = mysqli_connect($host, $user, $pass, $db);
-if (!$conn) { die("Connection failed: " . mysqli_connect_error()); }
-?>
